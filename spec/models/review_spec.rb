dcf5f06143fdc80dd9b19ca48bb8cd973941b400
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-07-05 19:53:10 -0700

    updated specs

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Review do
-  it { should validate_presence_of :author }
+  it { should validate_presence_of :author; :content; :rating }
+    it { should validate_numericality_of :rating }
+    it { should validate_length_of :content }
+
+    it { should belong_to :product }
+
 end
